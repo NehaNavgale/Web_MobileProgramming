54646d93231cd6aa304cd77eecd5df2791dda6ea
Neha Navgale <nn78d@mail.umkc.edu>
2019-09-28 15:24:19 -0500

    Added screenshots

--- ICP6/src/app/search-recipe/search-recipe.component.ts
+++ ICP6/src/app/search-recipe/search-recipe.component.ts
@@ -1,6 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-search-recipe',
@@ -41,19 +40,11 @@ export class SearchRecipeComponent implements OnInit {
         '&app_id=978afb53&app_key=3fa165424a2ddb8a9d8578a2a430e712').
           subscribe(respReceipe => {
             this.recipeList = respReceipe['hits'];
-            // this.recipeList = this.receipe.hits[0].recipe;
-            // if (this.receipe.hits.length === 0) {
-            //   Swal.fire('Please enter a food item to search');
-            // } else {
-            //   for (let i = 0; i < this.receipe.hits.length; i++) {
-            //     // this.recipeList(this.receipe.hits[i].recipe.url);
-            //   }
-            // }
           }, error => {});
 
     }
 
-    if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
+    if (this.placeValue != null && this.placeValue !== '') {
       this._http.get('https://api.foursquare.com/v2/venues/search?client_id=YFMKQYRTN2KHDPHHEWRDQLRFPDXUM31EN1HSCAYWTYYNJZWU' +
         '&client_secret=FWFROLKEYZPMTX2B15N0EOZN21VVJ5UJTCFX5O3XSRURHSS5&v=20190926&near=' + this.placeValue + '&query=restaurant').
       subscribe(respRestuarant => {
